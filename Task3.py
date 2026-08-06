9b685c86e8cf1ef680e123a4744514289543792c
Student ApnaCollege <student@apnacollege.in>
2026-08-06 14:05:49 +0530

    Updated Code

--- Task3.py
+++ Task3.py
@@ -1,6 +1,6 @@
-v1 = int(input("Enter value for v1: "))
-v2 = int(input("Enter value for v2: "))
-v3 = int(input("Enter value for v3: "))
+v1 = input("Enter value for v1: ")
+v2 = input("Enter value for v2: ")
+v3 = input("Enter value for v3: ")
 
 print("v1:", id(v1))
 print("v2:", id(v2))
