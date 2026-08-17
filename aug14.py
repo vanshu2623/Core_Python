cc9dad393a014081384822c920b0188decc9d96b
Student ApnaCollege <student@apnacollege.in>
2026-08-17 14:34:44 +0530

    Updated Code

---
+++ aug14.py
@@ -0,0 +1,140 @@
+# # Task 1
+# student_Name="Vanshika"
+# age=23
+# percentage=40
+
+# if percentage >= 50:
+#     print(student_Name ,"is Placed = True")
+# else:
+#     print(student_Name,"Not Placed = False")
+
+# print(type(student_Name))
+# print(type(age))  
+# print(type(percentage)) 
+
+# # Task 2
+# age = int(input("Enter a Age:"))
+
+# if (age+5):
+#     print(age)
+# else:
+#     print("Not age") 
+
+# # Task 4
+# number = [25,40,18,67,32]
+
+# print(min(number))
+# print(max(number))
+# print(sum(number))
+
+# # Task 4
+# a = 15
+# b = 5.87
+# c = 1+5j
+
+# print(a)
+# print(b)
+# print(c)
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# # Task 5
+# Full_Name = "Vanshika Vivek Akanpalliwar"
+# abs = -125
+# print(len(Full_Name))
+# print(abs)
+
+# # # Task 6
+# var = 78.4567
+# print(round(var))
+
+# # # Task 7
+# number = 5
+# print(pow(number,2))
+# print(pow(number,3))
+# print(pow(4,2))
+
+# # Task 8
+# num1 = int(input("Enter a Number1:"))
+# num2 = int(input("Enter a Number2"))
+
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/num2)
+# print(num1//num2)
+# print(num1%num2)
+# print(num1**num2)
+
+# # Task 9
+# num1 = 45
+# num2 = 15
+
+# print(num1>num2)
+# print(num1<num2)
+# print(num1==num2)
+# print(num1!=num2)
+# print(num1>=num2)
+# print(num1<=num2)
+
+# # Task 10
+# student_age = int(input("Enter a Age:"))
+# percentage = float(input("Enter a Percentage:"))
+
+# if student_age >= 18 and percentage >= 60:
+#     print("True")
+# else:
+#     print("False")    
+
+# # Task 11
+# x = 100
+# x+=20
+# print(x)
+# x-=20
+# print(x)
+# x*=20
+# print(x)
+# x/=5
+# print(x)
+
+# Task 12
+# marks = int(input("Enter a Marks:"))
+
+# if marks >=60:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")    
+
+# # Task 13
+# num1 = int(input("Enter a Number:"))
+
+# if num1 % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")    
+
+# Task 14
+# string = "KiranAcademy"
+# print(string[0])
+# print(string[-1])
+# print(string[4])
+# print(len(string))
+
+# Task 15
+# string = "Programming"
+# print(string[0:5])
+# print(string[-4:12])
+# print(string[::-1])
+
+# Task 16
+# string = "python full stack"
+# print(string.strip())
+# print(string.upper())
+# print(string.lower())
+# print(string.title())
+# print(string.replace("python","java"))
+# print(string)
+
+# Task 17
